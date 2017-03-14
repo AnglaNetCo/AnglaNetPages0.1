c0e5d55b732217bdbc0c3d5fda2630a028e27c56
GovjadkoIlja <govjadkoilja@yandex.ru>
2017-03-15 01:53:48 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/app/header/header.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-header',
+    templateUrl: './app/header/header.component.html',
+    styleUrls: ['./app/header/header.component.css']
+})
+export class HeaderComponent {
+    name: string;
+    selected: number;
+
+    constructor() {
+        //this.titles = ["Tasks", "Repetitor"];
+        this.name = "Ilya";
+        console.log("Hello");
+    }
+
+    select(n: number) {
+        this.name = "CLICK!";
+        this.selected = n;
+    }
+}
